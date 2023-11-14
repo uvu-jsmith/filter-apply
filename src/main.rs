9d6e23d0e2de56378b536d0a34ab17208697c97c
Jess Smith <10869841@uvu.edu>
2023-11-13 21:46:20 -0700

    added basic sepiatone function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use clap::Parser;
+use image::{DynamicImage, GenericImageView, ImageBuffer, Rgba};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    ifile: String,
+    ofile: String,
+}
+
+fn apply_sepia(image: DynamicImage) -> DynamicImage {
+    let (width, height) = image.dimensions();
+    let mut sepia_image = ImageBuffer::new(width, height);
+
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = image.get_pixel(x, y);
+
+            let rgba = pixel.0;
+            let r = rgba[0];
+            let g = rgba[1];
+            let b = rgba[2];
+            let a = rgba[3];
+
+            let new_r = (0.393 * r as f64 + 0.769 * g as f64 + 0.189 * b as f64).min(255.0) as u8;
+            let new_g = (0.349 * r as f64 + 0.686 * g as f64 + 0.168 * b as f64).min(255.0) as u8;
+            let new_b = (0.272 * r as f64 + 0.534 * g as f64 + 0.131 * b as f64).min(255.0) as u8;
+
+            let sepia_pixel = Rgba([new_r, new_g, new_b, a]);
+            sepia_image.put_pixel(x, y, sepia_pixel);
+        }
+    }
+
+    DynamicImage::ImageRgba8(sepia_image)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Opening {}", &args.ifile);
+    let input_image = image::open(&args.ifile).expect("Failed to open the input file");
+
+    let output_image = apply_sepia(input_image);
+
+    if args.ofile.is_empty() {
+        let output_filename = format!("sepia_{}", &args.ifile);
+        output_image.save(output_filename).expect("Failed to save the output file");
+    } else {
+        output_image.save(&args.ofile).expect("Failed to save the output file");
+    }
 }
