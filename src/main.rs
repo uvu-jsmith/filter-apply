f83b1ce86ce079596cadf83ec9becb0755d1ebb7
Jess Smith <10869841@uvu.edu>
2023-12-08 15:24:13 -0700

    clarification, finalization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use image::{DynamicImage, GenericImageView, ImageBuffer, Rgba, GrayImage, RgbaImage};
+use image::{DynamicImage, GenericImageView, GrayImage, ImageBuffer, Rgba, RgbaImage};
 
 #[derive(Parser, Debug)]
 #[command(author="Jess Smith", version, about="Simple image color filter program", long_about = None)]
@@ -49,13 +49,11 @@ struct ImageFilter {
 fn convert_to_dynamic(input: &GrayImage) -> DynamicImage {
     // Create a new RgbaImage to store the RGB values from the GrayImage
     let mut rgba_image = RgbaImage::new(input.width(), input.height());
-
     // Iterate over each pixel in the GrayImage and set the corresponding RGB value in the RgbaImage
     for (x, y, pixel) in rgba_image.enumerate_pixels_mut() {
         let luminance = input.get_pixel(x, y).0[0];
         *pixel = Rgba([luminance, luminance, luminance, 255]);
     }
-
     // Convert the RgbaImage to a DynamicImage
     DynamicImage::ImageRgba8(rgba_image)
 }
@@ -66,7 +64,7 @@ fn apply_filter(pre: ImageFilter) -> DynamicImage {
     let f_g = pre.g as f64;
     let f_b = pre.b as f64;
     let f_intensity = pre.intensity;
-    let gray = pre.image.to_luma8();
+    let gray = pre.image.to_luma8(); //Converts image to grayscale
     let gray_conv = convert_to_dynamic(&gray);
     let mut filtered_image = ImageBuffer::new(width, height);
 
@@ -80,9 +78,9 @@ fn apply_filter(pre: ImageFilter) -> DynamicImage {
             let b = rgba[2];
             let a = rgba[3];
 
-            let new_r = ((f_r/(255.0)) * r as f64 * f_intensity).min(255.0) as u8;
-            let new_g = ((f_g/(255.0)) * g as f64 * f_intensity).min(255.0) as u8;
-            let new_b = ((f_b/(255.0)) * b as f64 * f_intensity).min(255.0) as u8;
+            let new_r = ((f_r / (255.0)) * r as f64 * f_intensity).min(255.0) as u8;
+            let new_g = ((f_g / (255.0)) * g as f64 * f_intensity).min(255.0) as u8;
+            let new_b = ((f_b / (255.0)) * b as f64 * f_intensity).min(255.0) as u8;
 
             let filtered_pixel = Rgba([new_r, new_g, new_b, a]);
             filtered_image.put_pixel(x, y, filtered_pixel);
@@ -95,9 +93,17 @@ fn main() {
 
     println!("Opening {}", &args.ifile);
     let input_image = image::open(&args.ifile).expect("Failed to open the input file");
-    let pre_filter = ImageFilter{image:input_image, r:args.r, g:args.g, b:args.b, intensity:args.intensity};
+    let pre_filter = ImageFilter {
+        image: input_image,
+        r: args.r,
+        g: args.g,
+        b: args.b,
+        intensity: args.intensity,
+    };
     let output_image = apply_filter(pre_filter);
 
     let output_filename = format!("{}-filtered.jpg", &args.ifile);
-    output_image.save(output_filename).expect("Failed to save the output file");
+    output_image
+        .save(output_filename)
+        .expect("Failed to save the output file");
 }
